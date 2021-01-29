16f35dd01c92dd2a6c43c3be102132cd8a5f4e46
yzhang <zhang78@sheridancollege.ca>
2021-01-29 11:15:23 -0500

    change size to 2

--- src/students/StudentList.java
+++ src/students/StudentList.java
@@ -10,16 +10,13 @@ package students;
 public class StudentList {
     
     public static void main(String[] args){
-        Students[] sl = new Students[3];
+        Students[] sl = new Students[2];
         
         Students su1 = new Students();
         su1.setName("yan");
         
         Students su2 = new Students();
         su2.setName("yan2");
-        
-        Students su3 = new Students();
-        su3.setName("yan3");
         
        sl[0] = su1;
        sl[1] = su2;
